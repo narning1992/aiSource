2e56e24a521053ea5ec8c32f16a3f13e097bfacd
Nicolas Arning <39881226+narning1992@users.noreply.github.com>
2021-07-09 14:26:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# aiSource
+
+### Introduction
+Repository for the paper ["Machine learning to predict the source of campylobacteriosis using whole genome data"](https://doi.org/10.1101/2021.02.23.432443 )
+
+This repository should allow you to attribute sources of human campylobacter courses with the algorithm that we named **aiSource** after the previously most commonly used [iSource](https://doi.org/10.1371/journal.pgen.1000203) from core-genome multi-locus-sequence-type ([cgMLST](https://doi.org/10.1128/JCM.00080-17 )) data. The accuracy of this prediction should be ~84% based on realistic source composition in human cases. The different files in this repository do the following.
+
+* aiSource_prediction.py - The python script allowing you to predict sources
+* aiSource_classifier_object.p - A pickle object containing the XGBoost classifier trained in our paper
+* test_data.tsv - a file containing test cgMLST files in a tab delimited format
+* test_aiSource_out.tsv - the corresponding output file to the test data
+* venv - a folder containing the virtual environment for running the python script
+
+### Dataset acquisition
+Using aiSource you can easily predict any human campylobacter cgMLST samples. Campylobacter sources can be downloaded from the fantastic [pubmlst](pubmlst.org) database. Specifically for campylobacter [here](https://pubmlst.org/bigsdb?db=pubmlst_campylobacter_isolates&l=1&page=query). At the bottom of the page you can export the samples using the field "Dataset" next to the Export field with the little save icon. This brings you to the Export dataset field where you can choose as many provenance fields as you want just make sure you include id and under "Schemes" you choose All loci>Typing>C. Jejuni/C. coli cgMLST v1.0 as shown below.
+
+![Export dataset from PubMLST](./export_dataset.png)
+
+The resulting table should be saved with "Export table(text)" and can now be used for the prediction!
+
+If instead you want to use the dataset we used in the paper you can find it [here](https://pubmlst.org/bigsdb?db=pubmlst_campylobacter_isolates&page=query&project_list=102&submit=1). 
+
+Otherwise if you have whole genomes from campy you can upload a fasta of the genome get the corresponding cgMLST patterns [here](https://pubmlst.org/bigsdb?db=pubmlst_campylobacter_seqdef&l=1&page=batchSequenceQuery). Please choose the C. jejuni. C.coli cgMLST v1.0 in the "Please select locus/scheme" menu. The resulting table should again be saved with "Export table(text)".
+
+### Setting up the prediction
+
+
+### Source attribution using aiSource
+
+### Interpreting the output
+
---
+++ aiSource_prediction.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import pickle
+
+import xgboost
+import numpy as np
+import pandas as pd
+
+
+def load_data(input_data):
+    data = pd.read_csv(input_data, sep="\t", header=0)
+
+    # check if id column is in the data
+    if "id" not in data.columns:
+        print("ERROR\n No id column in data can't process the input. \nEXITING")
+        sys.exit()
+    else:
+        labels = data["id"]
+    # look for cgMLST columns
+    data = data.fillna(-1)
+    cgmlst_columns = [col for col in data if col.startswith('CAMP')]
+
+    data = data[cgmlst_columns]
+
+    # Find columns which have double alleles as indicated by a semicolon then
+    # replace those with NAs
+    columns_with_semicolon = data.columns[np.where(data.dtypes==object)[0]]
+    for col in columns_with_semicolon:
+        data[col] = pd.to_numeric(data[col], downcast="integer",errors="coerce")
+    data.fillna(-1, inplace=True)
+
+    # check if we have all 1343 cgMLST loci
+    if len(cgmlst_columns) < 1343:
+        print("""ERROR\n
+              Could not find 1,343 loci in the cgMLST data.
+              cgMLST loci start with CAMP in the pubmlst output data\n
+              EXITING""")
+        sys.exit()
+    return(labels, data.astype(int))
+
+
+def make_predictions(model,  data):
+    # define our source labels
+    source_dict = {
+        0: "cattle",
+        1: "chicken",
+        2: "environment",
+        3: "bird",
+        4: "sheep"
+        }
+
+    # make probabilistic assignment over all sources
+    source_prob = model.predict_proba(data)
+
+    # get highest probability
+    source_pred = list(np.argmax(source_prob, axis=1))
+    source_pred = [source_dict[x] for x in source_pred]
+    source_prob = list(source_prob.T)
+
+    # get headers for output
+    headers = ["probability_{}".format(source_dict[x]) for x in range(5)]
+
+    return (headers, source_pred, source_prob)
+
+
+def main():
+    # Script to use aiSource to attribue the source of human campylobacter
+    # infection based on the publication "Machine learning to predict the
+    # source of campylobacteriosis using whole genome data"
+
+    # read in command line arguments
+    parser = argparse.ArgumentParser(
+        description='aiSource source attribution of campylobacter cgMLSTs')
+    parser.add_argument('-i', type=str, help='dataset to be classified')
+    parser.add_argument(
+        '-n',
+        default="placeholder",
+        type=str,
+        help='give the output a prefix')
+    args = parser.parse_args()
+
+    # check if an output prefix has been specified if not take the input data
+    # as prefix
+    if args.n == "placeholder":
+        prefix = args.i.split(".")[0]
+    else:
+        prefix = args.n
+
+    print("Predicting the source on the file {} output will be stored in {}_aiSource_out.tsv".format(args.i, prefix))
+
+    # load in data which as it is downloaded from pubMLST
+    label, data = load_data(args.i)
+
+    # load in the trained XGBoost classifier
+    model=pickle.load(open('./aiSource_classifier_object.p', 'rb'))
+
+    # make predictions
+    headers, source_pred, source_probs=make_predictions(model, data)
+
+    # put prediction into dataframe for outputting
+    content_df=[list(label), list(source_pred)]
+    content_df += [ list(x) for x in source_probs]
+    column_headers=["id", "predicted source"]
+    column_headers += headers
+    df=pd.DataFrame(dict(zip(column_headers, content_df))).round(decimals=3)
+
+    # output dataframe
+    df.to_csv("{}_aiSource_out.tsv".format(prefix), sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
