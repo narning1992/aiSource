99fec179628114bcb4b5314b8ac5c5ee121852fb
Nicolas Arning <nicolas.arning@bdi.ox.ac.uk>
2021-07-12 13:52:27 +0100

    added retraining

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ### Introduction
 Repository for the paper ["Machine learning to predict the source of campylobacteriosis using whole genome data"](https://doi.org/10.1101/2021.02.23.432443 )
 
-This repository allows you to attribute sources of human campylobacter from core-genome multi-locus-sequence-type ([cgMLST](https://doi.org/10.1128/JCM.00080-17 )) data. The algorithm is named **aiSource** after the previously most commonly used [iSource](https://doi.org/10.1371/journal.pgen.1000203). The accuracy of this prediction should be ~84% based on realistic source composition in human cases, for a more thorough investigation of performance please consult the paper.
+This repository allows you to attribute sources of human campylobacter from core-genome multi-locus-sequence-type ([cgMLST](https://doi.org/10.1128/JCM.00080-17 )) data. The algorithm is named **aiSource** after the previously most commonly used [iSource](https://doi.org/10.1371/journal.pgen.1000203). The accuracy of this prediction should be ~84% based on realistic source composition in human cases, for a more thorough investigation of performance please consult the paper. We have also added a functionality by which you can retrain the classifier with more data, or if you wish train on other labels such as countries of origin or whatever you please.
 
 The different files in this repository do the following.
 
-* aiSource_prediction.py - The python script allowing you to predict sources
+* aiSource.py - The python script allowing you to predict sources
 * aiSource_classifier_object.p - A pickle object containing the XGBoost classifier trained in our paper
 * test_data.tsv - a file containing test cgMLST files in a tab delimited format
 * test_aiSource_out.tsv - the corresponding output file to the test data
@@ -32,7 +32,7 @@ In order to run aiSource you will need to install a few packages and make sure t
 
 for pip use 
 ```
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 ```
 (this is the most straightforward. See [here](https://pip.pypa.io/en/stable/installing/)
 for conda use
@@ -45,7 +45,8 @@ for virtualenv
 ```
     virtualenv --python=python3.8 aisource
     source aisource/bin/activate
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
+
 ```
 
 That should do the trick.
@@ -53,16 +54,30 @@ That should do the trick.
 ### Source attribution using aiSource
 Now that all packages are installed you can simply predict your cgMLST files like this
 ```
-    python3 aiSource_prediction.py -i <path to the cgMLST file you downloaded from pubMLST> -n <prefix for your output file>
+    python3 aiSource.py -i <path to the cgMLST file you downloaded from pubMLST> -n <prefix for your output file>
 ```
 You could do a test run with the data we provided like this:
 ```
-    python3 aiSource_prediction.py -i test_data.tsv -n test
+    python3 aiSource.py -i test_data.tsv -n test
 ```
-The output file will be created in the same folder under `<prefix>_aiSource_out.tsv`
+
+The output file will be created in the same folder under `<prefix>_aiSource_out.tsv`. Please be aware that rows with more than 10% missingness will be removed and won't show up in the output.
 
 ### Interpreting the output
 The output has 7 columns. The "id" columns contains the identifier specified in the input file under id. The "predicted_source" column contains the source predicted by aiSource. The following columns contain the predicted probabilities across all sources which sum up to 1.
 
 
+### Retraining the classifier
+We have also added a functionality to retrain the classifier with the same hyperparameter. You can now either feed in more samples for source attribution or other include more sources or predict a feature of your choosing. It's really simple. You will have to divide your data into two seperate tables for training and prediction. Of course you can also use the same file for training and prediction but there is little value in predicting what has been seen in training. 
 
+For training the classifier make sure you have a column in your training data that has the desired labels. As an example we could use the "country" column in the test_data.tsv. Make sure there are enough samples for every category within the label. The classifier will remove all labels with less than 15 members. You would then retrain the classifier like this
+```
+	python aiSource.py -rt country -i test_data.tsv -n country
+```
+
+The classifier is then retrained on 75% so we can use the residual 25% of samples for testing the accuracy which will be printed to the screen but also saved under `<prefix>_retraining_accurary_report.txt`. Subsequently all of the training data will be used to train the classifier and the classifier object will be stored under `<prefix>_retrained_clf.p`. We can then use this classifier to predict more data. In the case of our country classifier we would do it like so:
+```
+	python aiSource.py -clf country_retrained_clf.p -i test_data.tsv -n country
+```
+
+This example is quite useless as we are predicting the data we have already seen in training. However when used you should put in new cgMLST data and it will get predicted. Technically you could predict any label you can imagine from the cgMLST data an easily adapt this for other bugs than campylobacter. We hope this will be very useful to the community
---
+++ aiSource.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import pickle
+from collections import Counter
+
+import xgboost
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import classification_report
+
+
+def load_data(input_data, retrain_flag):
+    # load input cgMLST from tsv
+    data = pd.read_csv(input_data, sep="\t", header=0)
+
+    # check if id column is in the data
+    if "id" not in data.columns:
+        print("ERROR\n No id column in data can't process the input. \nEXITING")
+        sys.exit()
+    # look for cgMLST columns
+    cgmlst_columns = [col for col in data if col.startswith('CAMP')]
+    cgmlst_columns += ["id"]
+
+    # look whether we are retraining and retain the column on which we're
+    # retraining
+    if retrain_flag:
+        cgmlst_columns += [retrain_flag]
+    data = data[cgmlst_columns]
+
+    # Find columns which have double alleles as indicated by a semicolon then
+    # replace those with NAs
+    columns_with_semicolon = data.columns[np.where(data.dtypes == object)[0]]
+    for col in columns_with_semicolon:
+        if col != "id" and col != retrain_flag:
+            data[col] = pd.to_numeric(
+                data[col],
+                downcast="integer",
+                errors="coerce")
+
+    # drop rows with more than 10% missingness
+    before = data.shape[0]
+    data.dropna(thresh=134, axis=0, inplace=True)
+    print(
+    "Dropped {} rows because over 10% missingness. These won't show up in the output".format(
+        before -
+        data.shape[0]))
+    data.fillna(-1, inplace=True)
+
+    # get ids
+    ids = data["id"]
+    data.drop("id", axis=1, inplace=True)
+
+    if retrain_flag:
+        print("Retraining classifier with the label: {}".format(retrain_flag))
+        # get labels for retraining
+        labels = data[retrain_flag]
+        if len(set(labels)) <= 1:
+            print("ERROR\nThere is only one category in the label columns\nEXITING")
+            sys.exit()
+        # throw out labels with too gewe members
+        label_counts = dict(Counter(labels))
+        for label, label_count in label_counts.items():
+            if label_count <= 15:
+                print(
+    "Removing all samples with the label {} as there are only {} of them".format(
+        label, label_count))
+                data = data[data[retrain_flag] != label]
+        labels = data[retrain_flag]
+
+        data.drop(retrain_flag, axis=1, inplace=True)
+
+    else:
+        labels = None
+
+    # check if we have all 1343 cgMLST loci
+    if len(cgmlst_columns) < 1343:
+        print("""ERROR\n
+              Could not find 1,343 loci in the cgMLST data.
+              cgMLST loci start with CAMP in the pubmlst output data\n
+              EXITING""")
+        sys.exit()
+
+    return(ids, data.astype(int), labels)
+
+
+def retrain_clf(model, data, labels):
+    print("refitting model")
+
+    # encode lables as integers first then one-hot encode (dummy variable)
+    encoder = LabelEncoder()
+    labels = encoder.fit_transform(labels).reshape(-1, 1)
+    labels = OneHotEncoder(sparse=False).fit_transform(labels)
+    label_dict = dict(zip(range(len(encoder.classes_)), encoder.classes_))
+
+    # split randomly into training and testing
+    data_train, data_test, labels_train, labels_test = train_test_split(
+        data,
+        labels,
+        test_size=0.25,
+        stratify=labels)
+    model.fit(data_train, labels_train)
+    labels_predict = model.predict(data_test)
+
+    # get accuracy
+    print("Accuracy of the retrained model based on random 25% testing split")
+    accuracy_report = classification_report(
+            labels_test,
+            labels_predict,
+            target_names=label_dict.values()))
+    print(accuracy_report)
+    print("Now retraining with the whole data")
+    model.fit(data, labels)
+    print("Finished training")
+    return((label_dict, model), accuracy_report))
+
+
+def make_predictions(model,  data, label_dict):
+    # make predictions using the loaded XGBoost model
+
+    # make probabilistic assignment over all sources
+    source_prob=model.predict_proba(data)
+
+    # get highest probability
+    source_pred=list(np.argmax(source_prob, axis = 1))
+    source_pred=[label_dict[x] for x in source_pred]
+    source_prob=list(source_prob.T)
+
+    # get headers for output
+    headers=[
+        "probability_{}".format(label_dict[x])
+        for x in range(len(label_dict.keys()))]
+
+    return (headers, source_pred, source_prob)
+
+
+def main():
+    # Script to use aiSource to attribue the source of human campylobacter
+    # infection based on the publication "Machine learning to predict the
+    # source of campylobacteriosis using whole genome data"
+
+    # read in command line arguments
+    parser=argparse.ArgumentParser(
+        description='aiSource source attribution of campylobacter cgMLSTs')
+    parser.add_argument(
+        '-i',
+        '--input',
+        type=str,
+        help='dataset to be classified')
+    parser.add_argument(
+        '-n', '--name',
+        default=None,
+        type=str,
+        help='give the output a prefix')
+    parser.add_argument(
+        '-rt', '--retrain', default=None, type=str,
+        help='flag for retraining the classifier. Specify which column to train on')
+    parser.add_argument(
+        '-clf', '--classifier',
+        default="aiSource_classifier_object.p",
+        type=str,
+        help='''classifier object to do the prediction. Default is the aiSource predictor,
+        but if you have retrained the classifier with the rt flag you can specify
+        the resulting object here and use it for prediction''')
+
+    args=parser.parse_args()
+
+    # check if an output prefix has been specified if not take the input data
+    # as prefix
+    if args.name:
+        prefix=args.name
+    else:
+        prefix=args.input.split(".")[0]
+
+    print(
+        "Predicting the source on the file {} output will be stored in {}_aiSource_out.tsv".format(
+            args.input,
+            prefix))
+
+    # load in data which as it is downloaded from pubMLST
+    ids, data, labels=load_data(args.input, args.retrain)
+
+    # load in the trained classifier
+
+    if args.retrain:
+        # get initial classifier and retrain
+        label_dict, model=pickle.load(
+            open("aiSource_classifier_object.p", 'rb'))
+        new_model, accuracy_report=retrain_clf(model, data, labels)
+        with open("{}_retraining_accurary_report.txt".format(prefix), "w+") as outf:
+            outf.write(accuracy_report)
+
+        out_pickle="{}_retrained_clf.p".format(args.name)
+        print("""Dumping retrained classifier in {}\n If you want to use this
+              for prediction specify this object in the -clf flag in a rerun of
+              aisource""".format(out_pickle))
+        pickle.dump(new_model, open(out_pickle, "wb"))
+        print("DONE.EXITING")
+
+    else:
+        label_dict, model=pickle.load(open(args.classifier, 'rb'))
+        # make predictions
+        headers, source_pred, source_probs=make_predictions(
+            model, data, label_dict)
+
+        # put prediction into dataframe for outputting
+        content_df=[list(ids), list(source_pred)]
+        content_df += [list(x) for x in source_probs]
+        column_headers=["id", "predicion"]
+        column_headers += headers
+        df=pd.DataFrame(
+            dict(zip(column_headers, content_df))).round(
+            decimals=3)
+
+        # output dataframe
+        df.to_csv("{}_aiSource_out.tsv".format(prefix), sep="\t", index=False)
+        print("DONE.EXITING")
+
+
+if __name__ == "__main__":
+    main()
